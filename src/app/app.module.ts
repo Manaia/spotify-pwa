dbc3b6b854b10bb54e2ae5719de3c6b734a6eb12
Arthur Manaia <arthurnovais7@gmail.com>
2022-07-24 15:36:01 -0300

    Criando componente de login e arquivos de temas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+// Módulos angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Módulos sistema
+import { LoginModule } from './login/login.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [	
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
