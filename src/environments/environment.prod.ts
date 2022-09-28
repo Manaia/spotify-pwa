c7f4ec4f4b287538fdaf5a4f5340a277b40650f7
Arthur Manaia <arthurnovais7@gmail.com>
2022-09-28 11:37:22 -0300

    Adicionando o web push no front junto com o sw update para verificar novas atualizações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'http://localhost:9000',
+  VAPID_PUBLIC_KEY: 'BLsbmht6eyDpua2C2MFzR8nNDvu1TsDbvGtXlWxH5fsblxJdjOPwwqoc5xEBeqqF6NXf2J2Pk2QSG0MDHzRQFwA'
 };
