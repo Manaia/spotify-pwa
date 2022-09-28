a9805a5ed780cc4babd7258bda5b6d66e0b015a7
Arthur Manaia <arthurnovais7@gmail.com>
2022-09-28 15:33:53 -0300

    reajustando configuração de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'https://spotify-pwa-2a35-git-push-notification-manaia.vercel.app/',
+  API: 'https://spotify-pwa-2a35-git-push-notification-manaia.vercel.app',
   VAPID_PUBLIC_KEY: 'BHlYp-cHojnaGz0orlkYsUEs7Ra4dFLmjX3Vc19ef03IkwhpOI4NCTtM4aEDi8VKmLEyaW46dx80-sohTUmq1Fo'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'http://localhost:9000',
+  API: 'https://spotify-pwa-2a35-git-push-notification-manaia.vercel.app',
   VAPID_PUBLIC_KEY: 'BHlYp-cHojnaGz0orlkYsUEs7Ra4dFLmjX3Vc19ef03IkwhpOI4NCTtM4aEDi8VKmLEyaW46dx80-sohTUmq1Fo'
 };
 
