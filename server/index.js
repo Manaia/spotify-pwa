76553a5db0456884fbb7143fc77b8243cb0cd362
Arthur Manaia <arthurnovais7@gmail.com>
2022-09-28 15:29:00 -0300

    Ajustes de configuração do servidor

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const { adicionaPushSubscriber } = require('./adiciona-push-subscriber');
 
 // VAPID keys should be generated only once.
 const vapidKeys = {
-  publicKey: 'BLsbmht6eyDpua2C2MFzR8nNDvu1TsDbvGtXlWxH5fsblxJdjOPwwqoc5xEBeqqF6NXf2J2Pk2QSG0MDHzRQFwA',
-  privateKey: 'mZIxOID9kDTVJzH0dHM7UvMz_ue_GCyHYf9a5Giyz4A'
-};
+  publicKey:'BHlYp-cHojnaGz0orlkYsUEs7Ra4dFLmjX3Vc19ef03IkwhpOI4NCTtM4aEDi8VKmLEyaW46dx80-sohTUmq1Fo',
+  privateKey:'7r3P_k_FQjkYYEzcl9u2_tEPK7psiTfXw6lS6TSvHcY'
+}
 
 webpush.setVapidDetails(
   'mailto:example@yourdomain.org',
@@ -40,6 +40,7 @@ app.post('/api/notificacao/enviar', (req, res) => {
     vapidKeys.publicKey,
     vapidKeys.privateKey,
   );
+  
 
   let payload = JSON.stringify({
     "notification": {
